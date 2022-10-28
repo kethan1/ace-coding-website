6c9af16460ca6e432feb49dd0d47825431a075e5
Angad-The-Coder <angad.bhargav@gmail.com>
2022-10-27 23:19:00 -0700

    Update session 1 info

--- components/Calendar.vue
+++ components/Calendar.vue
@@ -11,42 +11,42 @@
     <div class="events">
       <Event
         class="event"
-        month="Feb"
-        :day="28"
+        month="Nov"
+        :day="2"
         time-range="3:30-4:30"
-        calendar-dates="20220228T233000Z%2F20220228T243000Z"
+        calendar-dates="20221102T233000Z%2F20221102T243000Z"
         event-title="HPMS - First Meeting of the Year!"
         event-location="Harvest Park Middle School"
         :event-description="
           'The first ever ACE Coding meeting at Harvest Park Middle School '
             + 'for the 2021-2022 school year.'"
-        sign-up-link="https://forms.gle/6ZpanzSSG9pbS1tN8"
+        sign-up-link="https://hpms.acecoding.org"
       />
       <Event
         class="event"
-        month="Mar"
-        :day="3"
-        time-range="3:45-4:45"
-        calendar-dates="20220303T234500Z%2F20220303T244500Z"
+        month="Nov"
+        :day="2"
+        time-range="3:35-4:35"
+        calendar-dates="20221102T233500Z%2F20221102T243500Z"
         event-title="PMS - First Meeting of the Year!"
         event-location="Pleasanton Middle School"
         :event-description="
           'The first ever ACE Coding meeting at Pleasanton Middle School '
             + 'for the 2021-2022 school year.'"
-        sign-up-link="https://forms.gle/Thzd1TEfCMA7iq7H8"
+        sign-up-link="https://pms.acecoding.org"
       />
       <Event
         class="event"
-        month="Mar"
+        month="Nov"
         :day="3"
         time-range="3:45-4:45"
-        calendar-dates="20220303T234500Z%2F20220303T244500Z"
+        calendar-dates="20221103T234500Z%2F20221103T244500Z"
         event-title="HMS - First Meeting of the Year!"
         event-location="Hart Middle School"
         :event-description="
           'The first ever ACE Coding meeting at Hart Middle School '
             + 'for the 2021-2022 school year.'"
-        sign-up-link="https://forms.gle/GKKCeGoRmLzBTZnTA"
+        sign-up-link="https://hms.acecoding.org"
       />
     </div>
   </div>
