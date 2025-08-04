70437182f8a38ccd412d30eb3e90e321a5499b5f
Kethan Vegunta <kethan@vegunta.com>
2025-08-04 15:00:12 -0700

    fix: page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "ACE Code Day",
+    title: "ACE Coding",
     htmlAttrs: {
       lang: "en",
       class: "scroll-smooth",
